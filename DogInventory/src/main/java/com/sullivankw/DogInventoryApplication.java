adfaf91db51ea1c8cf1cf366414cffda02bdfc41
Kevin Sullivan <sullivankw@gmail.com>
2019-05-22 18:28:31 -0700

    spring boot startup classes added

---
+++ DogInventory/src/main/java/com/sullivankw/DogInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.sullivankw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogInventoryApplication.class, args);
+	}
+
+}
